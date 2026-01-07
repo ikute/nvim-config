b9fdea84205103e5973dbd90b12b35715b5b3722
ione <ikutuzov.dev@gmail.com>
2026-01-07 14:18:26 +0300

    setup efm-langserver server

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -15,6 +15,7 @@ return {
             ensure_installed = {
                 "bash",
                 "c",
+                "go",
                 "html",
                 "json",
                 "lua",
