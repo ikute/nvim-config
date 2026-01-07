d3fb3ccc282fabfbf66ae12edca4886080ed2342
ione <ikutuzov.dev@gmail.com>
2026-01-07 19:28:05 +0300

    setup git integration plugins

---
+++ lua/plugin/gitsigns.lua
@@ -0,0 +1,10 @@
+-- =====================================
+-- @path: <plugin/gitsigns.lua>
+-- @brief: Git integration
+-- @see: https://github.com/lewis6991/gitsigns.nvim
+-- =====================================
+
+return {
+    "lewis6991/gitsigns.nvim",
+    opts = {},
+}
---
+++ lua/plugin/vim-fugitive.lua
@@ -0,0 +1,9 @@
+-- =====================================
+-- @path: <plugin/vim-fugitive.lua>
+-- @brief: Powerful git integration and commands
+-- @see: https://github.com/tpope/vim-fugitive
+-- =====================================
+
+return {
+    "tpope/vim-fugitive",
+}
