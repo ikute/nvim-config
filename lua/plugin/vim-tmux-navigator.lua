36aeba9daf76b5d865246cf8cadcb815e2c9d708
ione <ikutuzov.dev@gmail.com>
2026-01-09 17:43:10 +0300

    setup vim-tmux-navigator plugin

---
+++ lua/plugin/vim-tmux-navigator.lua
@@ -0,0 +1,9 @@
+-- =====================================
+-- @path: <plugin/vim-tmux-navigator.lua>
+-- @brief: Navigation between tmux panes and vim splits
+-- @see: https://github.com/christoomey/vim-tmux-navigator
+-- =====================================
+
+return {
+    "christoomey/vim-tmux-navigator",
+}
